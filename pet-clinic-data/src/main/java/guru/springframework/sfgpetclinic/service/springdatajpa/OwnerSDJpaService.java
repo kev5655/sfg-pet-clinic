da264b69d6ad569a19eb70b2e379700c0aab5ebb
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-01-30 18:05:38 +0100

    added spring data jpa service for owners. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,61 @@
+package guru.springframework.sfgpetclinic.service.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repo.OwnerRepo;
+import guru.springframework.sfgpetclinic.repo.PetRepo;
+import guru.springframework.sfgpetclinic.repo.PetTypeRepo;
+import guru.springframework.sfgpetclinic.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepo ownerRepo;
+    private final PetRepo petRepo;
+    private final PetTypeRepo petTypeRepo;
+
+    public OwnerSDJpaService(OwnerRepo ownerRepo, PetRepo petRepo, PetTypeRepo petTypeRepo) {
+        this.ownerRepo = ownerRepo;
+        this.petRepo = petRepo;
+        this.petTypeRepo = petTypeRepo;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepo.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        Optional<Owner> optionalOwner = ownerRepo.findById(aLong);
+        return optionalOwner.orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepo.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepo.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepo.deleteById(aLong);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepo.findByLastName(lastName);
+    }
+}
