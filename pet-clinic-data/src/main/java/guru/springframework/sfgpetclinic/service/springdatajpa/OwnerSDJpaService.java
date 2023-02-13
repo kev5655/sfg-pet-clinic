ddf3b412687e336198efcb585ccc86615b76a101
kevin <kevin.zahn@bluewin.ch>
2023-02-13 13:07:49 +0100

    impl find owner. Closes #43

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/OwnerSDJpaService.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -24,6 +25,11 @@ public class OwnerSDJpaService implements OwnerService {
         this.ownerRepo = ownerRepo;
         this.petRepo = petRepo;
         this.petTypeRepo = petTypeRepo;
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return ownerRepo.findAllByLastNameLike(lastName);
     }
 
     @Override
@@ -58,4 +64,6 @@ public class OwnerSDJpaService implements OwnerService {
     public Owner findByLastName(String lastName) {
         return ownerRepo.findByLastName(lastName);
     }
+
+
 }
