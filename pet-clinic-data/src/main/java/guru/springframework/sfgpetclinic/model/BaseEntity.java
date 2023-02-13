454a9b5c67033ebdd5c3d425fafdb17824ae05c4
kevin <kevin.zahn@bluewin.ch>
2023-02-13 13:53:43 +0100

    Added create update owner. Closes #44

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -22,4 +22,8 @@ public class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    public boolean isNew(){
+        return this.id == null;
+    }
+
 }
